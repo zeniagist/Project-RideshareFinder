a23336dd8ad98a843108e074467499f0331d12da
Zenia Gist <zeniagist@gmail.com>
2020-12-28 16:36:29 +0000

    stored geocode for destination and departure when selecting Add Trip

--- mytrips.js
+++ mytrips.js
@@ -1,3 +1,6 @@
+// create a geocoder object to use geocode
+var geocoder = new google.maps.Geocoder();
+
 // Hide all the date and time checkbox inputs
 $(".regular").hide();
 $(".oneoff").hide();
@@ -47,4 +50,69 @@ $('input[name="date"], input[name="date2"]').datepicker({
     maxDate: "+12M",
     showWeek: true
 });
+ var data, departureLongitude, departureLatitude, destinationLongitude, destinationLatitude;
+// Click on Create Trip Button
+$("#addtripform").submit(function(event){
+    //prevent default php processing
+    event.preventDefault();
+    
+    //collect user inputs
+    data = $(this).serializeArray();
+    //   console.log(data);
+    
+    getAddTripDepartureCoordinates();
+});
 
+// Store Departure Coordinates
+function getAddTripDepartureCoordinates(){
+    // get coordinates of departure
+    geocoder.geocode({
+        'address': $("#departure").val()
+    }, 
+    function(results, status){
+        if(status == google.maps.GeocoderStatus.OK){
+            // console.log(results);
+            
+            // get latitude and logitude
+            departureLongitude = results[0].geometry.location.lng();
+            departureLatitude = results[0].geometry.location.lat();
+            // add to data array
+            data.push({name: 'departureLongitude', value: departureLongitude});
+            data.push({name:'departureLatitude', value: departureLatitude});
+            
+            // console.log(data);
+            
+            getAddTripDestinationCoordinates();
+        }else{
+            // coordinates of destination
+            getAddTripDestinationCoordinates();
+        }
+    });
+}
+
+// Store Desitination Coordinates
+function getAddTripDestinationCoordinates(){
+    // get coordinates of departure
+    geocoder.geocode({
+        'address': $("#destination").val()
+    }, 
+    function(results, status){
+        if(status == google.maps.GeocoderStatus.OK){
+            // console.log(results);
+            
+            // get latitude and logitude
+            destinationLongitude = results[0].geometry.location.lng();
+            destinationLatitude = results[0].geometry.location.lat();
+            // add to data array
+            data.push({name: 'destinationLongitude', value: destinationLongitude});
+            data.push({name:'destinationLatitude', value: destinationLatitude});
+            
+            // console.log(data);
+            
+            // getAddTripDepartureCoordinates();
+        }else{
+            // coordinates of depature
+            // getAddTripDepartureCoordinates();
+        }
+    });
+}
