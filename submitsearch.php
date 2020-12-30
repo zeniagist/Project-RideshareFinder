d349df6fe58ef298d798645a342c8edba87307d1
Zenia Gist <zeniagist@gmail.com>
2020-12-30 04:26:30 +0000

    added longitude coordinates to find search radius

--- submitsearch.php
+++ submitsearch.php
@@ -49,4 +49,61 @@ if($errors){
     echo $resultMessage;
     exit;
 }
+
+// set search radius
+$searchRadius = 10; // 10 miles
+
+// ******
+// Departure Longitude
+// *******
+// longitude out of range
+$departureLngOutOfRange = false;
+
+// Departure Longitude
+$deltaLongitudeDeparture = ($searchRadius * 360)/(24901*cos(deg2rad($departureLatitude)));
+
+// min Departure Longitude
+$minLongitudeDeparture = $departureLongitude - $deltaLongitudeDeparture;
+if($minLongitudeDeparture < -180){
+    $departureLngOutOfRange = true;
+    $minLongitudeDeparture += 360;
+}
+
+// max Departure Longitude
+$maxLongitudeDeparture = $departureLongitude + $deltaLongitudeDeparture;
+if($maxLongitudeDeparture > 180){
+    $departureLngOutOfRange = true;
+    $minLongitudeDeparture -= 360;
+}
+
+// ******
+// Destination Longitude
+// *******
+// longitude out of range
+$destinationLngOutOfRange = false;
+
+// Destination Longitude
+$deltaLongitudeDestination = ($searchRadius * 360)/(24901*cos(deg2rad($destinationLatitude)));
+
+// min Destination Longitude
+$minLongitudeDestination = $destinationLongitude - $deltaLongitudeDeparture;
+if($minLongitudeDestination < -180){
+    $destinationLngOutOfRange = true;
+    $minLongitudeDestination += 360;
+}
+
+// max Destination Longitude
+$maxLongitudeDestination = $destinationLongitude + $deltaLongitudeDeparture;
+if($maxLongitudeDestination > 180){
+    $destinationLngOutOfRange = true;
+    $minLongitudeDestination -= 360;
+}
+
+
+
+
+
+
+
+
 ?>
