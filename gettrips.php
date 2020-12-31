8b5fa7e4cdce88087b7b8479a306289a7e2a618c
Zenia Gist <zeniagist@gmail.com>
2020-12-31 02:41:09 +0000

    changed message when the are no trips

--- gettrips.php
+++ gettrips.php
@@ -1,50 +1,57 @@
 <?php
+//start session and connect
 session_start();
 include('connection.php');
 
-$user_id = $_SESSION['user_id'];
+//retrieve all trips
+$sql="SELECT * FROM carsharetrips WHERE user_id='".$_SESSION['user_id']."'";
 
-// selet all trips from database
-$sql = "SELECT * FROM carsharetrips WHERE user_id='$user_id'";
-$result = mysqli_query($link, $sql);
-
-if(!$result){
-  echo '<div class="alert alert-danger">Error running the get trip query!</div>';
-  exit;
-}
-// print_r($result);
-
-$count = mysqli_num_rows($result);
-
-if($count <= 0){
-  echo '<br /><div class="alert alert-warning">You have not created any trips yet!</div>';
-  exit;
-}
-
-$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-
-while($row){
-    // check frequency
-    if($row['regular']=="N"){
-        // one-off trip
-        $frequency = "One-off Journey";
-        $time = $row['date'] . " at " . $row['time'];
+if($result = mysqli_query($link, $sql)){
+    //print_r($result);
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_array($result)){
+            //check frequency
+            if($row['regular']=="N"){
+                $frequency = "One-off journey";
+                $time = $row['date']." at " .$row['time'];
+            }else{
+                $frequency = "Regular Journey"; 
+                $array = [];
+                    if($row['monday']==1){array_push($array,"Mon");}
+                    if($row['tuesday']==2){array_push($array,"Tue");}
+                    if($row['wednesday']==3){array_push($array,"Wed");}
+                    if($row['thursday']==4){array_push($array,"Thu");}
+                    if($row['friday']==5){array_push($array,"Fri");}
+                    if($row['saturday']==6){array_push($array,"Sat");}
+                    if($row['sunday']==7){array_push($array,"Sun");}
+                $time = implode("-", $array)." at " .$row['time'];
+            }
+            $departure = $row['departure'];
+            $destination = $row['destination'];
+            $price = $row['price'];
+            $seatsavailable = $row['seatsavailable'];
+            $trip_id = $row['trip_id'];
+            
+            echo 
+             '<div class="row trip">
+                    <div class="col-sm-8 journey">
+                        <div><span class="departure">Departure:</span> '.$departure.'.</div>
+                        <div><span class="destination">Destination:</span> '. $destination .'.</div>
+                        <div class="time">'.$time.'</div>
+                        <div>'.$frequency.'</div>
+                    </div>
+                    <div class="col-sm-2 price">
+                        <div class="price">$'.$price.'</div>
+                        <div class="perseat">Per Seat</div>
+                        <div class="seatsavailable">'.$seatsavailable.' left</div>
+                    </div>
+                    <div class="col-sm-2">
+                        <button class= "btn purple edit btn-lg" data-target="#edittripModal" data-toggle="modal" data-trip_id="'.$trip_id.'">Edit</button>
+                    </div>
+                </div>';
+        }
     }else{
-        $frequency = "Regular Journey";
-        $array = [];
-        
-        // check days
-        if($row['monday']==1){array_push($array, "Mon");}
-        if($row['tuesday']==2){array_push($array, "Tue");}
-        if($row['wednesday']==3){array_push($array, "Wed");}
-        if($row['thursday']==4){array_push($array, "Thu");}
-        if($row['friday']==5){array_push($array, "Fri");}
-        if($row['saturday']==6){array_push($array, "Sat");}
-        if($row['sunday']==7){array_push($array, "Sun");}
-        
-        // dashes between days
-        $time = implode("-", $array) . " at " . $row['time'];
+        echo '<div class="notrips alert alert-warning">Start an adventure, share your Rideshare!</div>';
     }
 }
-
 ?>
